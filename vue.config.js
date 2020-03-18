a44eca4e6b2329128db7f65efc96789c693909be
XuJiazhen <xu.159623@gmail.com>
2020-03-19 06:30:49 +0800

    init vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,20 @@
+import * as path from 'path';
+
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+
 module.exports = {
   publicPath: '/',
   outputDir: 'dist',
   assetsDir: 'static',
+  lintOnSave: process.env.NODE_ENV !== 'production',
+  productionSourceMap: false,
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': resolve('src'),
+      },
+    },
+  },
 };

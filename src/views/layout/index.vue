0b79adb4c61f1a51cf6d77b3c6c2660761065f84
XuJiazhen <xu.159623@gmail.com>
2020-03-21 16:01:01 +0800

    init layout dir

---
+++ src/views/layout/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="layout-container">
+    <div class="sidebar-container"></div>
+    <div class="main-container">
+      <div class="header"></div>
+      <div class="main"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Layout',
+};
+</script>
+
+<style scoped lang="less"></style>
